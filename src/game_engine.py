76a848160f98e0cb90416f9b1aaaa383cff800e9
iamprinx <geek_200@outlook.com>
2019-06-09 19:50:07 +0100

    refactor code base

--- src/game_engine.py
+++ src/game_engine.py
@@ -7,7 +7,7 @@ from board import Board
 #************************** Needed global variable ************************** #
 
 turns = 1               # turns which denotes which player's turn it is to play
-gameFlag = True         # while be used to control how game is played
+gameFlag = True         # will be used to signify if game has started or ended
 
 
 pOne = pTwo = playingPlayer = None
@@ -143,50 +143,50 @@ def display_draw(  ):
     sys.exit( 0 )
 
 
-def compute_input_received( i_recieved, board_obj ):
+def compute_input_received( player_input, board_obj ):
     '''
     call appropriate method based on the input received
     '''
 
     # updates the current column position by moving it to the right and
     # re-display board
-    if i_recieved.lower().startswith('r'):
-        col_num = board_obj.move_column_to_right(i_recieved)
-        row_num = board_obj.get_current_row( )
+    if player_input.lower().startswith('r'):
+        column_number = board_obj.move_column_to_right(player_input)
+        row_number = board_obj.get_current_row( )
         display_top_info( )
-        board_obj.set_rNc_position(col_num, row_num)
+        board_obj.set_rNc_position(column_number, row_number)
 
 
     # updates the current column position by moving it to the left and
     # re-display board
-    if i_recieved.lower().startswith('l'):
-        col_num = board_obj.move_column_to_left(i_recieved)
-        row_num = board_obj.get_current_row( )
+    if player_input.lower().startswith('l'):
+        column_number = board_obj.move_column_to_left(player_input)
+        row_number = board_obj.get_current_row( )
         display_top_info( )
-        board_obj.set_rNc_position(col_num, row_num)
+        board_obj.set_rNc_position(column_number, row_number)
 
 
     # updates the current row position by moving it down and re-display board
-    if i_recieved.lower().startswith('u'):
-        col_num = board_obj.get_current_column( )
-        row_num = board_obj.move_row_up(i_recieved )
+    if player_input.lower().startswith('u'):
+        column_number = board_obj.get_current_column( )
+        row_number = board_obj.move_row_up(player_input )
         display_top_info( )
-        board_obj.set_rNc_position(col_num, row_num)
+        board_obj.set_rNc_position(column_number, row_number)
 
 
     # updates the current row position by moving it down and re-display board
-    if i_recieved.lower().startswith('d'):
-        col_num = board_obj.get_current_column( )
-        row_num = board_obj.move_row_down(i_recieved )
+    if player_input.lower().startswith('d'):
+        column_number = board_obj.get_current_column( )
+        row_number = board_obj.move_row_down(player_input )
         display_top_info( )
-        board_obj.set_rNc_position(col_num, row_num)
+        board_obj.set_rNc_position(column_number, row_number)
 
 
     # add player mark to current row and col position only if that position
     # has not been marked before. A user is given another chance to mark an
     # empty position in case mark was not placed
 
-    if i_recieved.lower().startswith('m'):
+    if player_input.lower().startswith('m'):
         was_marked = False
         err_msg = ''
         playingPlayer = get_playing_player( )
@@ -194,8 +194,8 @@ def compute_input_received( i_recieved, board_obj ):
         # only if a player still have move, then a player will be able to
         # mark a position
         was_marked = board_obj.mark_rNc_position(playingPlayer.playerMark)
-        row_num = board_obj.get_current_row( )
-        col_num = board_obj.get_current_column( )
+        row_number = board_obj.get_current_row( )
+        column_number = board_obj.get_current_column( )
 
         # only if the cell was marked then we want to reduce players
         # move count and also swap players turn
@@ -207,13 +207,13 @@ def compute_input_received( i_recieved, board_obj ):
 
             if won:
                 display_top_info( )
-                board.set_rNc_position(col_num, row_num)
+                board.set_rNc_position(column_number, row_number)
                 display_winner( board_obj, playingPlayer, winPattern)
 
             # if there is a draw
             if not won and board.get_total_marks_on_board( ) == 9:
                 display_top_info( )
-                board.set_rNc_position(col_num, row_num)
+                board.set_rNc_position(column_number, row_number)
                 display_draw( )
 
 
@@ -230,7 +230,7 @@ def compute_input_received( i_recieved, board_obj ):
             )
 
         else:
-            board_obj.set_rNc_position(col_num, row_num)
+            board_obj.set_rNc_position(column_number, row_number)
 
 
 
@@ -264,5 +264,5 @@ while( gameFlag ):
     # receive inputs ( compute for navigation
     # `l` --- left              `r` --- right
     # `u` --- up                `d` --- down
-    recieved_input = input("\n\n>>>  ")
-    compute_input_received( recieved_input, board )
+    player_input = input("\n\n>>>  ")
+    compute_input_received( player_input, board )
